9678e22c613ae09e8468fa536984f0e2ab14ddc6
Jan Nyman <jan.nyman@controlthings.fi>
2018-01-21 23:03:34 +0200

    Header file for utility

---
+++ source/mist_port/inet_aton.h
@@ -0,0 +1,13 @@
+/*
+ * inet_aton.h
+ *
+ *  Created on: Jan 21, 2018
+ *      Author: jan
+ */
+
+#ifndef SOURCE_MIST_PORT_INET_ATON_H_
+#define SOURCE_MIST_PORT_INET_ATON_H_
+
+int inet_aton(const char *cp, struct in_addr *addr);
+
+#endif /* SOURCE_MIST_PORT_INET_ATON_H_ */
